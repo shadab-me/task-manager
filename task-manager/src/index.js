c4544d9ccde092ac6ca77e780c40b247cb5c5735
Shadab Ali <shadabali604@gmail.com>
2021-01-03 22:48:38 +0530

    added: new project

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,30 +1,26 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+const jwt = require("jsonwebtoken");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+  console.log("Server is up on port " + port);
+});
 
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
 
 const myFunction = async () => {
-    const password = 'Red12345!'
-    const hashedPassword = await bcrypt.hash(password, 8)
+  jwt.sign({ id: "0001233" }, "this", function (err, result) {
+    console.log(result, 1);
+  });
+};
 
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('red12345!', hashedPassword)
-    console.log(isMatch)
-}
-
-myFunction()
+myFunction();
